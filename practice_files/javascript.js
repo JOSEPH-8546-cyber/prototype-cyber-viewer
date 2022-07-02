2174c24d4975fc2e3740d694bc56559d7230d32f
Joseph <manueljosephtrabajo@gmail.com>
2022-07-01 19:31:32 -0600

    Add files of practice html, css, javascript

---
+++ practice_files/javascript.js
@@ -0,0 +1,18 @@
+/*
+start practice code of javascript
+*/
+
+change = () => {
+  document.getElementById("cw").innerHTML = "Hello Mr.White";
+}
+
+/* function of date and time */
+function date() {
+  document.getElementById("ce").innerHTML = Date();
+}
+
+/* function of change styles*/
+style = () => {
+  document.getElementById("cr").style.color = "red";
+  document.getElementById("cr").style.backgroundColor = "yellow";
+}
